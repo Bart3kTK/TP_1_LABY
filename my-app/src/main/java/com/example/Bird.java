c88c0db64983ff471d1c081f421c3644d35a1fcb
Bart3kTK <bkamieniarz55@wp.pl>
2023-10-17 20:22:00 +0200

    elo

--- my-app/src/main/java/com/example/Bird.java
+++ my-app/src/main/java/com/example/Bird.java
@@ -4,7 +4,7 @@ public class Bird extends Protist{
 
     public void sayHi()
     {
-        System.out.println("Hello world! I'm Bird, I can fly :D");
+            System.out.println("Hello world! I'm Bird, I can fly :D" + a);
     }
     
 }
